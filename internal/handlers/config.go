1d22ca2c7594fa37007067d8a982aaaed36da6c3
AgungAryansyah <agungaryansyah2006@gmail.com>
2026-07-08 13:11:04 +0700

    feat: add public config API endpoint

---
+++ internal/handlers/config.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"research-data-collection/internal/config"
+)
+
+type publicConfig struct {
+	ChunkDurationMs int      `json:"chunkDurationMs"`
+	VideoBitrate    int      `json:"videoBitrate"`
+	MaxWidth        int      `json:"maxWidth"`
+	MaxHeight       int      `json:"maxHeight"`
+	InfoFields      []string `json:"infoFields"`
+}
+
+func ConfigHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	c := config.Get()
+	pub := publicConfig{
+		ChunkDurationMs: c.ChunkDurationMs,
+		VideoBitrate:    c.VideoBitrate,
+		MaxWidth:        c.MaxWidth,
+		MaxHeight:       c.MaxHeight,
+		InfoFields:      c.InfoFields,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(pub)
+}
